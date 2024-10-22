7d7be7fba638658dc389b16ef9b8552135d2aad3
Thanh Tuấn <tuanndt76@outlook.com>
2024-10-22 14:11:23 +0700

    Create pages, and index for pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, Project, SignIn, SignUp } from "./pages/index";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold">Hello Arys Domi!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ Frontend/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as About } from "./About";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
+export { default as Project } from "./Project";
+export { default as Home } from "./Home";
