207f55bbf9827299164b94910b134c7a3c1ccbb7
Thanh Tuấn <tuanndt76@outlook.com>
2024-10-31 20:01:37 +0700

    Connect database with MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,38 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Server Start
 const app = express();
-
 const PORT = process.env.PORT;
-
 app.listen(PORT, () =>
   console.log(`MERN Blog App | Backend started on port ${PORT}`)
 );
+
+// Mongo Database Connection
+const MongoDB_Username = process.env.MONGO_Username;
+const MongoDB_Password = process.env.MONGO_Password;
+const MongoDB_Cluster = process.env.MONGO_Cluster;
+const MongoDB_Database = process.env.MONGO_Database;
+const MongoDB_URL = `mongodb+srv://${MongoDB_Username}:${MongoDB_Password}@${MongoDB_Cluster}.mongodb.net/?retryWrites=true&w=majority&appName=${MongoDB_Database}`;
+
+mongoose
+  .connect(MongoDB_URL)
+  .then(() => {
+    console.log("Mongoose Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+// Maybe using another connection
+// const MongoDB_Connection = async () => {
+//   try {
+//     await mongoose.connect(MongoDB_URL);
+//     console.log("Mongoose Connected");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
