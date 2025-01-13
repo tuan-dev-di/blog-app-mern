b9ce36e637b69f2cce34910d72d56e8348fdccab
Thanh Tuấn <tuanndt76@outlook.com>
2025-01-14 01:43:34 +0700

    Update UI for title and category in CreatePost page

--- Frontend/src/pages/posts/CreatePost.jsx
+++ Frontend/src/pages/posts/CreatePost.jsx
@@ -48,28 +48,38 @@ const CreatePost = () => {
         <div className="flex flex-col gap-4 justify-between">
           {/* -------------------- TITLE & CATEGORY -------------------- */}
           <div>
-            <Label className="text-base">
-              Title and Category<span className="text-red-500 ml-1">*</span>
-            </Label>
-            <div className="flex gap-2 sm:flex-row">
-              <TextInput
-                id="title"
-                type="text"
-                className="flex-1"
-                placeholder="Enter a title"
-                require
-              />
-              <Select>
-                <option value="uncategorized">
-                  ----- Language | Framework -----
-                </option>
-                <option value="javascript-reactjs">JavaScript | ReactJS</option>
-                <option value="javascript-vuejs">JavaScript | VueJS</option>
-                <option value="javascript-nodejs">JavaScript | NodeJS</option>
-                <option value="golang-gin">GoLang | Gin</option>
-                <option value="golang-echo">GoLang | Echo</option>
-                <option value="golang-fiber">GoLang | Fiber</option>
-              </Select>
+            <div className="flex gap-4 sm:flex-row items-start">
+              <div className="flex flex-col flex-1">
+                <Label className="text-base">
+                  Title<span className="text-red-500 ml-1">*</span>
+                </Label>
+                <TextInput
+                  id="title"
+                  type="text"
+                  className="flex-1 w-[450px]"
+                  placeholder="Enter a title"
+                  required
+                />
+              </div>
+
+              <div className="flex flex-col flex-1">
+                <Label className="text-base">
+                  Title<span className="text-red-500 ml-1">*</span>
+                </Label>
+                <Select id="category" className="flex-1">
+                  <option value="uncategorized">
+                    ----- Language | Framework -----
+                  </option>
+                  <option value="javascript-reactjs">
+                    JavaScript | ReactJS
+                  </option>
+                  <option value="javascript-vuejs">JavaScript | VueJS</option>
+                  <option value="javascript-nodejs">JavaScript | NodeJS</option>
+                  <option value="golang-gin">GoLang | Gin</option>
+                  <option value="golang-echo">GoLang | Echo</option>
+                  <option value="golang-fiber">GoLang | Fiber</option>
+                </Select>
+              </div>
             </div>
           </div>
 
@@ -78,12 +88,6 @@ const CreatePost = () => {
             <Label className="text-base">
               Content<span className="text-red-500 ml-1">*</span>
             </Label>
-            {/* <Textarea
-              id="comment"
-              placeholder="Enter your content"
-              required
-              rows={7}
-            /> */}
             <ReactQuill
               theme="snow"
               placeholder="Enter your content"
@@ -96,53 +100,6 @@ const CreatePost = () => {
             <Label className="text-base">
               Image<span className="text-red-500 ml-1">*</span>
             </Label>
-            {/* <div className="w-full items-center justify-center">
-              <Label
-                htmlFor="dropzone-file"
-                className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                  {selectedImage ? (
-                    <img
-                      src={selectedImage}
-                      alt="Selected"
-                      className="h-full w-full object-cover rounded-lg"
-                    />
-                  ) : (
-                    <>
-                      <svg
-                        className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 16"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                        />
-                      </svg>
-                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span className="font-semibold">Click to upload</span>{" "}
-                        or drag and drop
-                      </p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">
-                        SVG, PNG, JPG or GIF (MAX. 800x400px)
-                      </p>
-                    </>
-                  )}
-                </div>
-                <FileInput
-                  id="dropzone-file"
-                  className="hidden"
-                  accept="image/*"
-                  onChange={handleChangeImagePost}
-                />
-              </Label>
-            </div> */}
             <div className="w-full items-center justify-center">
               <Label
                 htmlFor="dropzone-file"
