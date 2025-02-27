e1ac0265c845eff2fd9f633fd5f8e984dde70805
Thanh Tuấn <tuanndt76@outlook.com>
2025-02-28 00:24:55 +0700

    Update Auth

--- Backend/middleware/Auth.js
+++ Backend/middleware/Auth.js
@@ -1,28 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-// const verify_token = (req, res, next) => {
-//   const authHeader = req.header("Authorization");
-//   const token = authHeader && authHeader.split(" ")[1];
-
-//   if (!token)
-//     return res.status(401).json({ success: false, message: "Access Denied" });
-
-//   try {
-//     const decodedToken = jwt.verify(token, process.env.Access_Token);
-//     req.userId = decodedToken.userId;
-//     next();
-//   } catch (error) {
-//     console.log("ERROR: ", error);
-//     return res.status(403).json({
-//       success: false,
-//       message: "Invalid Token",
-//     });
-//   }
-// };
-
 const verifyToken = async (req, res, next) => {
   //? ---------------| CHECK HAS TAKEN TOKEN |---------------
-  const token = req.cookies.accessToken;
+  const token =
+    req.cookies.accessToken || req.header("Authorization").split(" ")[1];
   if (!token)
     return res.status(401).json({ success: false, message: "Access Denied" });
 
@@ -33,6 +14,7 @@ const verifyToken = async (req, res, next) => {
         return res
           .status(401)
           .json({ success: false, message: "Access Denied" });
+
       req.user = {
         userId: user.userId,
         role: user.role,
