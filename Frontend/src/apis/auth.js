6f9c823f21a4efcde8f5b82992728e5e745206bd
Thanh Tuấn <tuanndt76@outlook.com>
2025-06-19 17:23:58 +0700

    Update: Fixed API

--- Frontend/src/apis/auth.js
+++ Frontend/src/apis/auth.js
@@ -1,8 +1,8 @@
 import { callApi } from "./base";
 
 export const GOOGLE_AUTH = (data) =>
-  callApi("/api/auth/users/google-auth", "POST", data);
-export const SIGN_IN = (data) => callApi("/api/auth/users/sign-in", "POST", data);
-export const SIGN_UP = (data) => callApi("/api/auth/users/sign-up", "POST", data);
+  callApi("/auth/users/google-auth", "POST", data);
+export const SIGN_IN = (data) => callApi("/auth/users/sign-in", "POST", data);
+export const SIGN_UP = (data) => callApi("/auth/users/sign-up", "POST", data);
 export const SIGN_OUT = (userId) =>
-  callApi(`/api/auth/users/sign-out/${userId}`, "POST");
+  callApi(`/auth/users/sign-out/${userId}`, "POST");
--- Frontend/src/apis/user.js
+++ Frontend/src/apis/user.js
@@ -1,13 +1,13 @@
 import { callApi } from "./base";
 
 export const UPDATE_ACCOUNT = (userId, data) =>
-  callApi(`/api/auth/users/account/update/${userId}`, "PUT", data);
+  callApi(`/auth/users/account/update/${userId}`, "PUT", data);
 export const DELETE_ACCOUNT = (userId, data) =>
-  callApi(`/api/auth/users/account/delete/${userId}`, "DELETE", data);
+  callApi(`/auth/users/account/delete/${userId}`, "DELETE", data);
 
 export const GET_USERS = async (userId, page, limit) => {
   const response = await callApi(
-    `/api/auth/users/get-users/${userId}?page=${page}&limit=${limit}`,
+    `/auth/users/get-users/${userId}?page=${page}&limit=${limit}`,
     "POST"
   );
   return response?.data;
@@ -15,7 +15,7 @@ export const GET_USERS = async (userId, page, limit) => {
 
 export const GET_USERS_OVERVIEW = async (userId, limit) => {
   const response = await callApi(
-    `/api/auth/users/get-users/${userId}?limit=${limit}`,
+    `/auth/users/get-users/${userId}?limit=${limit}`,
     "POST"
   );
   return response?.data;
